9f6de3fd3feb5a3438817bf8e3f0736b740ca827
Enrique Perez <28enriqueperez@gmail.com>
2020-12-04 15:34:16 -0400

    feat: create base components

---
+++ src/components/Question.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Question(){
+    return(
+        <div>
+            <h2>Título de la pregunta</h2>
+            <button>Opcion 1</button>
+            <button>Opcion 2</button>
+            <button>Opcion 3</button>
+            <button>Opcion 4</button>
+        </div>
+    )
+}
+
+export default Question;
