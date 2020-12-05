1ccb7394008219f09cdee5edf58af21e1737f31d
Enrique Perez <28enriqueperez@gmail.com>
2020-12-05 11:37:36 -0400

    feat: implement Question component logic

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import Question from './components/Question';
 
 function App(){
 
+  const [isLoading, setIsLoading] = useState(true);
   const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([])
 
   useEffect(()=>{
    
     async function loadData(){
       const response = await fetchQuestions();
       const formatedQuestion = formatQuestions(response.data.results);
-      setQuestions(formatedQuestion);
+      setQuestions(formatedQuestion); 
+      setIsLoading(false);
     }
 
     loadData();
   }, [])
+
+  const saveUserAnswer = (userAnswer) => {
+    setUserAnswers([...userAnswers, userAnswer])
+  }
+
+  const removeUserAnswer = (id) => {
+    const updatedAnswers = userAnswers.filter(answer => answer.questionID !== id);
+    setUserAnswers(updatedAnswers);
+  }
+
 
   return (
     <div>
@@ -24,9 +37,18 @@ function App(){
         <div>
           <h1>QuizBee</h1>
         </div>
-        {questions.map(question => (
-          <Question key={question.id} id={question.id} question={question.question} answers={question.answers}/>
+        {isLoading ? <h2>Loading...</h2> : questions.map(question => (
+          <Question 
+            key={question.id} 
+            id={question.id} 
+            question={question.question} 
+            answers={question.answers} 
+            saveUserAnswer={saveUserAnswer}
+            removeUserAnswer={removeUserAnswer}/>
         ))}
+        <div>
+          <button>Check results</button>
+        </div>
       </main>
     </div>
   );
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function Question(props){
+    
+    const [displayedAnswers, setDisplayedAnswers] = useState(props.answers) //(!) Passing props to the state?
+
+    const handleUserSelection = (e) => {
+        const {index, parent_id} = e.target.attributes; 
+        setDisplayedAnswers(previosAnswers => [previosAnswers[index.value]]);
+        
+        const answerObject = {
+            questionID : parent_id.value,
+            answer: e.target.value
+        }
+
+        props.saveUserAnswer(answerObject); //We also save the user's answer in the state.
+    }
+
+    const handleReset = (e) => {
+        const {parent_id} = e.target.attributes;
+        setDisplayedAnswers(props.answers); //We render all of the answer options again
+        props.removeUserAnswer(parent_id.value); //We also remov user's answer from the state
+    }
+
+
     return(
         <div>
             <p>{props.question}</p>
-            {props.answers.map(answer => (
-                <button key={uuidv4()}>{answer}</button>
-            ))}
+            <div>
+                {displayedAnswers.map((answer, index) => (
+                    <button key={uuidv4()} index={index} parent_id={props.id} value={answer} onClick={handleUserSelection}>{answer}</button>
+                ))}
+            </div>
+            <button parent_id={props.id} onClick={handleReset}>Reset</button>
         </div>
     )
 }
